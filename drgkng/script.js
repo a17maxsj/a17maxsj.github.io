57f5b55c9174f63a85788ae4f7b4ec6422aa6cdd
a17maxsj <a17maxsj@student.his.se>
2019-05-03 16:49:19 +0200

    ll

---
+++ drgkng/script.js
@@ -0,0 +1,27 @@
+var hp = 0;
+var aspirationvalue = 1;
+var aspiratedhp = 0;
+gearboxvalue = 0;
+
+function engine(value) {
+    hp = value;
+    document.getElementById("hp").innerHTML = "HP: " +
+        hp;
+    console.log("engine selected");
+
+}
+
+function aspiration(value) {
+    aspirationvalue = value;
+    aspiratedhp = hp * value;
+    document.getElementById("hp").innerHTML = "HP: " +
+        Math.trunc(aspiratedhp);
+
+    console.log(aspiratedhp, value);
+}
+
+function gearbox(value) {
+    gearboxvalue = value;
+    document.getElementById("gears").innerHTML = "Gears: " + gearboxvalue;
+
+}
