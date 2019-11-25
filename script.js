3a803aed430ff60d733718e1ad9743f554f418c5
Max Sjöstedt <49199993+a17maxsj@users.noreply.github.com>
2019-11-25 17:11:59 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-var checked = false;
+var drum = new Audio('sounds/ricercivic.mp3');
+var d24 = new Audio('sounds/d24.mp3');
+var civic = new Audio('sounds/vtec.mp3');
+var v8 = new Audio('sounds/v8.mp3');
 
-function myFunction() {
 
-    if (checked === false) {
-        document.getElementById("menu").style.height = "200px";
-        checked = true;
-    } else {
-        document.getElementById("menu").style.height = "50px";
-        checked = false;
-    }
-
+function playDrum(){
+    drum.play();
 }
+function playD24(){
+    d24.play();
+}
+
+function playCivic(){
+    civic.play();
+}
+
+function playV8(){
+    v8.play();
+}
+
+
+document.addEventListener("keypress", function onEvent(event) {
+    if (event.key === "f") {
+        drum.play();
+    }
+    else if (event.key === "g") {
+        d24.play();
+    }
+    else if (event.key === "h") {
+        civic.play();
+    }
+    else if (event.key === "j") {
+        v8.play();
+    }
+});
